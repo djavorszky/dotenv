1606952aaca35f96dd3c2985a3f7e7209ce737fe
Daniel Javorszky <daniel.javorszky@gmail.com>
2022-02-14 23:02:27 +0100

    initial commit

---
+++ scripts/install-ext.sh
@@ -0,0 +1,25 @@
+# oh-my-zsh
+if [ ! -e ~/.oh-my-zsh ]; then
+  echo "installing oh-my-zsh"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+
+
+# Install node:
+if [ ! -e ~/apps/node ]; then
+  echo "installing node"
+  mkdir -p ~/apps
+  cd ~/apps
+
+  curl -fsSL https://nodejs.org/dist/v17.5.0/node-v17.5.0-linux-x64.tar.xz -o node.tar.gz
+
+  tar xf node.tar.gz
+  rm node.tar.gz
+  mv node-* node
+fi
+
+# set git stuff
+git config --global user.name "Daniel Javorszky"
+git config --global user.email daniel.javorszky@gmail.com
+git config --global core.editor "vim"
