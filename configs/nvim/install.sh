aa248eef20e1f99fb33bb23506676a2515d8505c
Daniel Javorszky <daniel.javorszky@gmail.com>
2022-10-28 18:39:15 +0200

    Rust LSP config

--- configs/nvim/install.sh
+++ configs/nvim/install.sh
@@ -1,7 +1,6 @@
 mkdir -p ~/.config/nvim
-mkdir -p ~/.config/nvim/user
+mkdir -p ~/.config/nvim
 
 dirname=$(dirname $(realpath $0))
 cp $dirname/init.lua ~/.config/nvim
-cp $dirname/rust.lua ~/.config/nvim
 
