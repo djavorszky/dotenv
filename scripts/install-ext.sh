88fde741fdf8ee667d684c2cbf9c61745c709897
Daniel Javorszky <daniel.javorszky@gmail.com>
2022-10-30 19:41:26 +0100

    Rebase when pull by default

--- scripts/install-ext.sh
+++ scripts/install-ext.sh
@@ -72,3 +72,4 @@ fi
 git config --global user.name "Daniel Javorszky"
 git config --global user.email daniel.javorszky@gmail.com
 git config --global core.editor "nvim"
+git config --global pull.rebase true
