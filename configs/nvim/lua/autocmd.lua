adda10c0e0b687ec032ed9713828912f2fe8030c
Daniel Javorszky <daniel.javorszky@gmail.com>
2022-10-30 19:40:03 +0100

    format on save

--- configs/nvim/lua/autocmd.lua
+++ configs/nvim/lua/autocmd.lua
@@ -4,3 +4,14 @@ vim.cmd([[
     autocmd BufWritePost *.sh | ShellCheck! 
   augroup end
 ]])
+
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*.rs",
+	callback = function()
+		vim.lsp.buf.formatting_sync(nil, 200)
+	end,
+	group = format_sync_grp,
+})
+
+
