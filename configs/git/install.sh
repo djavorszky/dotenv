b0a77ae7e7dea987f3b5cb9a5b839184fe8ab29e
Daniel Javorszky <daniel.javorszky@gmail.com>
2022-10-30 19:43:25 +0100

    Move git to configs

---
+++ configs/git/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git config --global user.name "Daniel Javorszky"
+git config --global user.email daniel.javorszky@gmail.com
+git config --global core.editor "nvim"
+git config --global pull.rebase true
--- scripts/install-ext.sh
+++ scripts/install-ext.sh
@@ -65,11 +65,3 @@ if [ ! -e $APPS/bin/rust-analyzer ]; then
   chmod +x $APPS/bin/rust-analyzer
 fi
 
-
-
-
-# set git stuff
-git config --global user.name "Daniel Javorszky"
-git config --global user.email daniel.javorszky@gmail.com
-git config --global core.editor "nvim"
-git config --global pull.rebase true
